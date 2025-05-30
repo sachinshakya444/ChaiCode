7393a74252a1931f7b2c205aba46be17f5b3e662
Sachin Shakya <sachinshakya613@gmail.com>
2025-05-30 12:00:57 +0530

    Array methods

---
+++ 02_array_object.js
@@ -0,0 +1,8 @@
+let chaiTypes = ["Masala Chai","Ginger chai","Green Tea","Lemon Tea"];
+console.log(chaiTypes[2]);
+console.log(`Total chai Types: ${chaiTypes.length}`);
+console.log(chaiTypes)
+for(let i=0;i<chaiTypes.length;i++)
+{
+    console.log(`chai at index ${i} is ${chaiTypes[i]}`);
+}
