ccbf1069eb04af3afa757cd44725a88348c8dc34
Sachin Shakya <sachinshakya613@gmail.com>
2025-06-01 09:18:27 +0530

    done with accessing and concate array and object

--- 02_array_object.js
+++ 02_array_object.js
@@ -6,3 +6,42 @@ for(let i=0;i<chaiTypes.length;i++)
 {
     console.log(`chai at index ${i} is ${chaiTypes[i]}`);
 }
+chaiTypes.push("Herbal Tea");
+const data = chaiTypes.pop();
+console.log(data);
+let index = chaiTypes.indexOf("Green-Tea");
+console.log(index);
+if(index!= -1)
+{
+    chaiTypes.splice(index,1);
+}
+
+console.log(chaiTypes);
+
+chaiTypes.forEach((chai,index)=>{
+    console.log(`${index+1}: ${chai}`);
+})
+
+let moreChaiTypes = ["Oolong Tea","White Tea"];
+
+let allChaiTypes = chaiTypes.concat(moreChaiTypes);
+
+console.log(allChaiTypes);
+
+let newChaiTypes = [...chaiTypes,"Chamolina Tea"];
+
+console.log(newChaiTypes);
+
+let chaiRecipe = {
+    name: "Masala Chai",
+    ingredients: {
+        teaLeaves: "Assam Tea",
+        milk: "Full Cream Milk",
+        sugar: "Brown sugar",
+        spices: ["Daalchini","Ginger"],
+    },
+    instruction: "Boil water, add tea leaves, milk, sugar and spices"
+};
+
+console.log(chaiRecipe.ingredients.spices[1]);
+
